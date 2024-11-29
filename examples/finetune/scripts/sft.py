325cd7671e217e1d35b391e0a2d5c63330ad6107
LoserCheems <3314685395@qq.com>
2024-11-29 14:46:32 +0800

    update finetune configs

--- examples/finetune/scripts/sft.py
+++ examples/finetune/scripts/sft.py
@@ -1,52 +1,85 @@
-# TODO: 着手准备SFT的全训练流程
+import os
+import logging
+from argparse import ArgumentParser
 
-
+import yaml
 from datasets import load_from_disk
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from trl import SFTConfig, SFTTrainer
 
-dataset = load_from_disk('./datasets/Infinity-Instruct_gen_processed')
-dataset['train'] = dataset['train'].select(range(1000))
 
 if __name__ == "__main__":
+
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument('--pretrained_model_name_or_path', type=str, default='JingzeShi/Doge-76M', help='pretrained model name or path')
+    arg_parser.add_argument('--config_path', type=str, default='./examples/pretrain/configs/doge_197M.yaml', help='path to yaml config file')
+    arg_parser.add_argument('--logging_dir', type=str, default='logs')
+    arg_parser.add_argument('--output_dir', type=str, default='results')
+    arg_parser.add_argument("--resume_from_checkpoint", type=str, default=None, help="resume from checkpoint")
+
+    args = arg_parser.parse_args()
+
+    with open(args.config_path, 'r', encoding='utf-8') as f:
+        hyperparameters = yaml.load(f, Loader=yaml.FullLoader)
+
+    model_name = args.config_path.split('/')[-1].split('.')[0]
+    logging_dir = f'{args.logging_dir}/{model_name}'
+    output_dir = f'{args.output_dir}/{model_name}'
+
+    os.makedirs(logging_dir, exist_ok=True)
+    logging.basicConfig(filename=f'{logging_dir}/log.log', level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    console_handler = logging.StreamHandler()
+    logger.addHandler(console_handler)
+
+    # 加载数据集
+    # Load dataset
+    dataset = load_from_disk(hyperparameters['training_args']['dataset_path'])
+    dataset["train"] = dataset["train"].select(range(hyperparameters['training_args']['per_epoch_max_steps'] * hyperparameters['training_args']['per_device_train_batch_size'] * hyperparameters['training_args']['gradient_accumulation_steps']))
+
+    # 加载模型
+    # Load model
+    tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model_name_or_path)
+    model = AutoModelForCausalLM.from_pretrained(args.pretrained_model_name_or_path, trust_remote_code=True)
+
+    num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    logger.info(model)
+    logger.info(f"Model parameters: {num_params}")
+
+    # 监督微调参数
+    # supervised finetuning argements
     sft_config = SFTConfig(
         seed=233,
-        logging_dir="./logs",
-        logging_steps=100,
-        output_dir="./results",
+        logging_dir=logging_dir,
+        logging_steps=hyperparameters['training_args']['logging_steps'],
+        output_dir=output_dir,
 
         do_train=True,
-        num_train_epochs=1,
-        per_device_train_batch_size=1,
+        num_train_epochs=hyperparameters['training_args']['num_train_epochs'],
+        per_device_train_batch_size=hyperparameters['training_args']['per_device_train_batch_size'],
         
         do_eval=True,
         eval_strategy="steps",
-        eval_steps=1000,
-        per_device_eval_batch_size=1,
-        dataset_num_proc=1,
-
-        do_predict=True,
+        eval_steps=hyperparameters['training_args']['eval_steps'],
+        per_device_eval_batch_size=hyperparameters['training_args']['per_device_eval_batch_size'],
+        dataset_num_proc=hyperparameters['training_args']['dataset_num_proc'],
         
-        warmup_ratio=0.1,
-        learning_rate=6e-4,
-        weight_decay=0.01,
+        learning_rate=hyperparameters['training_args']['learning_rate'],
+        warmup_ratio=hyperparameters['training_args']['warmup_ratio'],
         lr_scheduler_type="cosine_with_min_lr",
-        lr_scheduler_kwargs={'min_lr_rate': 0.1},
+        lr_scheduler_kwargs={'min_lr_rate': hyperparameters['training_args']['min_lr_rate']},
+        weight_decay=hyperparameters['training_args']['weight_decay'],
 
         save_safetensors=True,
         save_strategy="steps",
-        save_steps=1000,
+        save_steps=hyperparameters['training_args']['save_steps'],
 
-        bf16=False,
-        max_grad_norm=1.0,
-        gradient_accumulation_steps=32,
-        max_seq_length=2048,
-        num_of_sequences=1,
-        packing=False,
+        bf16=hyperparameters['training_args']['bf16'],
+        max_grad_norm=hyperparameters['training_args']['max_grad_norm'],
+        gradient_accumulation_steps=hyperparameters['training_args']['gradient_accumulation_steps'],
+        max_seq_length=hyperparameters['training_args']['max_seq_length'],
+        packing=hyperparameters['training_args']['packing'],
     )
-
-    tokenizer = AutoTokenizer.from_pretrained("JingzeShi/Doge-76M")
-    model = AutoModelForCausalLM.from_pretrained("JingzeShi/Doge-76M", trust_remote_code=True)
 
     trainer = SFTTrainer(
         model=model,
@@ -56,4 +89,4 @@ if __name__ == "__main__":
         processing_class=tokenizer,
     )
 
-    trainer.train()
+    trainer.train(resume_from_checkpoint=args.resume_from_checkpoint)
--- examples/pretrain/scripts/pretrain.py
+++ examples/pretrain/scripts/pretrain.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     arg_parser.add_argument('--logging_dir', type=str, default='logs')
     arg_parser.add_argument('--output_dir', type=str, default='results')
     arg_parser.add_argument('--tokenizer_path', type=str, default='./examples/tokenizer', help='path to tokenizer')
-    arg_parser.add_argument("--resume_from_checkpoint", type=str, default=None, help="./results/doge_197M/checkpoint-13800")
+    arg_parser.add_argument("--resume_from_checkpoint", type=str, default=None, help="resume from checkpoint")
 
     args = arg_parser.parse_args()
 
@@ -36,72 +36,72 @@ if __name__ == '__main__':
 
     # 加载数据集
     # Load dataset
-    dataset = load_from_disk(hyperparameters['train']['dataset_path'])
-    dataset["train"] = dataset["train"].select(range(hyperparameters['train']['per_epoch_max_steps'] * hyperparameters['train']['per_device_train_batch_size'] * hyperparameters['train']['gradient_accumulation_steps']))
+    dataset = load_from_disk(hyperparameters['training_args']['dataset_path'])
+    dataset["train"] = dataset["train"].select(range(hyperparameters['training_args']['per_epoch_max_steps'] * hyperparameters['training_args']['per_device_train_batch_size'] * hyperparameters['training_args']['gradient_accumulation_steps']))
 
     # 加载模型
     # Load model
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_path)
     config = DogeConfig(
-        vocab_size=hyperparameters['model']['vocab_size'],
-        hidden_size=hyperparameters['model']['hidden_size'],
-        intermediate_size=hyperparameters['model']['intermediate_size'],
-        num_hidden_layers=hyperparameters['model']['num_hidden_layers'],
-        hidden_bias=hyperparameters['model']['hidden_bias'],
-        hidden_dropout=hyperparameters['model']['hidden_dropout'],
-        hidden_act=hyperparameters['model']['hidden_act'],
-        max_position_embeddings=hyperparameters['model']['max_position_embeddings'],
-        rope_theta=hyperparameters['model']['rope_theta'],
-        use_cache=hyperparameters['model']['use_cache'],
-        pad_token_id=hyperparameters['model']['pad_token_id'],
-        bos_token_id=hyperparameters['model']['bos_token_id'],
-        eos_token_id=hyperparameters['model']['eos_token_id'],
-        num_attention_heads=hyperparameters['model']['num_attention_heads'],
-        num_inner_values=hyperparameters['model']['num_inner_values'],
-        num_inner_value_heads=hyperparameters['model']['num_inner_value_heads'],
-        num_value_per_head=hyperparameters['model']['num_value_per_head'],
-        inner_values_retrieval_size=hyperparameters['model']['inner_values_retrieval_size'],
-        private_expert_retrieval_size=hyperparameters['model']['private_expert_retrieval_size'],
-        num_cdmmoe_experts=hyperparameters['model']['num_cdmmoe_experts'],
-        num_cdmmoe_heads=hyperparameters['model']['num_cdmmoe_heads'],
-        num_cdmmoe_experts_per_head=hyperparameters['model']['num_cdmmoe_experts_per_head'],
+        vocab_size=hyperparameters['model_config']['vocab_size'],
+        hidden_size=hyperparameters['model_config']['hidden_size'],
+        intermediate_size=hyperparameters['model_config']['intermediate_size'],
+        num_hidden_layers=hyperparameters['model_config']['num_hidden_layers'],
+        hidden_bias=hyperparameters['model_config']['hidden_bias'],
+        hidden_dropout=hyperparameters['model_config']['hidden_dropout'],
+        hidden_act=hyperparameters['model_config']['hidden_act'],
+        max_position_embeddings=hyperparameters['model_config']['max_position_embeddings'],
+        rope_theta=hyperparameters['model_config']['rope_theta'],
+        use_cache=hyperparameters['model_config']['use_cache'],
+        pad_token_id=hyperparameters['model_config']['pad_token_id'],
+        bos_token_id=hyperparameters['model_config']['bos_token_id'],
+        eos_token_id=hyperparameters['model_config']['eos_token_id'],
+        num_attention_heads=hyperparameters['model_config']['num_attention_heads'],
+        num_inner_values=hyperparameters['model_config']['num_inner_values'],
+        num_inner_value_heads=hyperparameters['model_config']['num_inner_value_heads'],
+        num_value_per_head=hyperparameters['model_config']['num_value_per_head'],
+        inner_values_retrieval_size=hyperparameters['model_config']['inner_values_retrieval_size'],
+        private_expert_retrieval_size=hyperparameters['model_config']['private_expert_retrieval_size'],
+        num_cdmmoe_experts=hyperparameters['model_config']['num_cdmmoe_experts'],
+        num_cdmmoe_heads=hyperparameters['model_config']['num_cdmmoe_heads'],
+        num_cdmmoe_experts_per_head=hyperparameters['model_config']['num_cdmmoe_experts_per_head'],
     )
     config.vocab_size = tokenizer.vocab_size
     model = DogeForCausalLM(config=config)
 
     num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     logger.info(model)
-    logger.info(num_params)
+    logger.info(f"Model parameters: {num_params}")
 
     # 训练参数
     training_args = TrainingArguments(
         seed=233,
         logging_dir=logging_dir,
-        logging_steps=hyperparameters['train']['logging_steps'],
+        logging_steps=hyperparameters['training_args']['logging_steps'],
         output_dir=output_dir,
 
         do_train=True,
-        num_train_epochs=hyperparameters['train']['num_train_epochs'],
-        per_device_train_batch_size=hyperparameters['train']['per_device_train_batch_size'],
+        num_train_epochs=hyperparameters['training_args']['num_train_epochs'],
+        per_device_train_batch_size=hyperparameters['training_args']['per_device_train_batch_size'],
 
         do_eval=True,
         eval_strategy="steps",
-        eval_steps=hyperparameters['train']['eval_steps'],
-        per_device_eval_batch_size=hyperparameters['train']['per_device_eval_batch_size'],
+        eval_steps=hyperparameters['training_args']['eval_steps'],
+        per_device_eval_batch_size=hyperparameters['training_args']['per_device_eval_batch_size'],
 
-        learning_rate=hyperparameters['train']['learning_rate'],
-        warmup_ratio=hyperparameters['train']['warmup_ratio'],
+        learning_rate=hyperparameters['training_args']['learning_rate'],
+        warmup_ratio=hyperparameters['training_args']['warmup_ratio'],
         lr_scheduler_type="cosine_with_min_lr",
-        lr_scheduler_kwargs={'min_lr_rate': hyperparameters['train']['min_lr_rate']},
-        weight_decay=hyperparameters['train']['weight_decay'],
+        lr_scheduler_kwargs={'min_lr_rate': hyperparameters['training_args']['min_lr_rate']},
+        weight_decay=hyperparameters['training_args']['weight_decay'],
         
         save_safetensors=True,
         save_strategy="steps",
-        save_steps=hyperparameters['train']['save_steps'],
+        save_steps=hyperparameters['training_args']['save_steps'],
 
-        bf16=hyperparameters['train']['bf16'],
-        max_grad_norm=hyperparameters['train']['max_grad_norm'],
-        gradient_accumulation_steps=hyperparameters['train']['gradient_accumulation_steps'],
+        bf16=hyperparameters['training_args']['bf16'],
+        max_grad_norm=hyperparameters['training_args']['max_grad_norm'],
+        gradient_accumulation_steps=hyperparameters['training_args']['gradient_accumulation_steps'],
     )
 
     data_collator = DataCollatorForLanguageModeling(
