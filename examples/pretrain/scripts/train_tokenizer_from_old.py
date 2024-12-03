3b6830829d85ff782a017435b158ccbf4ce5a297
LoserCheems <3314685395@qq.com>
2024-12-03 21:58:20 +0800

    updated train tokenizer from old LlaMa2 tokenizer

--- examples/pretrain/scripts/train_tokenizer_from_old.py
+++ examples/pretrain/scripts/train_tokenizer_from_old.py
@@ -20,14 +20,10 @@ def main(args):
     # 计算fineweb-edu, cosmopedia-v2, python-edu, open-web-math的大小
     # Calculate the size of fineweb-edu, cosmopedia-v2, python-edu, open-web-math
     fineweb_edu_ratio, cosmopedia_v2_ratio, python_edu_ratio, open_web_math_ratio = 0.7, 0.2, 0.05, 0.05
-    fineweb_edu_size = int(args.examples * fineweb_edu_ratio)
-    cosmopedia_v2_size = int(args.examples * cosmopedia_v2_ratio)
-    python_edu_size = int(args.examples * python_edu_ratio)
-    open_web_math_size = int(args.examples * open_web_math_ratio)
-
-    # 加载分词器
-    # Load tokenizer
-    old_tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_path)
+    fineweb_edu_size = int(args.num_examples * fineweb_edu_ratio)
+    cosmopedia_v2_size = int(args.num_examples * cosmopedia_v2_ratio)
+    python_edu_size = int(args.num_examples * python_edu_ratio)
+    open_web_math_size = int(args.num_examples * open_web_math_ratio)
 
     # 采样 fineweb-edu
     # Sample fineweb-edu
@@ -49,6 +45,7 @@ def main(args):
     # Sample cosmopedia-v2
     cosmopedia_v2 = load_from_disk(args.datasets_dir + '/cosmopedia-v2')
     cosmopedia_v2 = cosmopedia_v2.shuffle(seed=233)
+    column_names = cosmopedia_v2.column_names
     cosmopedia_v2 = cosmopedia_v2.select(
         range(cosmopedia_v2_size)
     ).map(
@@ -63,6 +60,7 @@ def main(args):
     # Sample python-edu
     python_edu = load_from_disk(args.datasets_dir + '/python-edu')
     python_edu = python_edu.shuffle(seed=233)
+    column_names = python_edu.column_names
     python_edu = python_edu.select(
         range(python_edu_size)
     ).map(
@@ -77,6 +75,7 @@ def main(args):
     # Sample open-web-math
     open_web_math = load_from_disk(args.datasets_dir + '/open-web-math')
     open_web_math = open_web_math.shuffle(seed=233)
+    column_names = open_web_math.column_names
     open_web_math = open_web_math.select(
         range(open_web_math_size)
     ).map(
@@ -90,15 +89,25 @@ def main(args):
     # 合并样本
     # Concatenate samples
     dataset = concatenate_datasets([fineweb_edu, cosmopedia_v2, python_edu, open_web_math])
+    dataset.save_to_disk(args.datasets_dir + './datasets/tokenizer_examples')
+
+    dataset = load_from_disk(args.datasets_dir + '/tokenizer_examples')
+    dataset = dataset.select(range(args.num_examples))
+   
 
     def get_training_corpus():
-        for i in range(0, args.examples, 1000):
-            samples = dataset[i : i + 1000]["examples"]
+        for i in range(0, len(dataset), 1):
+            samples = dataset[i : i + 1]["examples"]
             yield samples
     
+    # 加载分词器
+    # Load tokenizer
+    old_tokenizer = AutoTokenizer.from_pretrained(args.old_tokenizer_path)
+
     # 训练分词器
     # Train tokenizer
-    new_tokenizer = old_tokenizer.train_new_from_iterator(get_training_corpus, vocab_size=32768)
+    training_corpus = get_training_corpus()
+    new_tokenizer = old_tokenizer.train_new_from_iterator(training_corpus, vocab_size=32768)
 
     # 保存新分词器
     # Save new tokenizer
@@ -111,7 +120,7 @@ if __name__ == '__main__':
     argparser.add_argument("--old_tokenizer_path", type=str, default="./examples/tokenizer")
     argparser.add_argument("--new_tokenizer_save_dir", type=str, default="./examples/tokenizer_new")
     argparser.add_argument("--num_examples", type=int, default=1_000_000)
-    argparser.add_argument("--num_proc", type=int, default=4)
+    argparser.add_argument("--num_proc", type=int, default=8)
     args = argparser.parse_args()
 
     main(args)
