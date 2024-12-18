3190c3e7f5bd5f466f0cc1b298c6946d4558417a
LoserCheems <3314685395@qq.com>
2024-12-18 12:14:37 +0800

    updated eval downstream tasks scripts

--- examples/evaluate/eval_downstream_tasks.ps1
+++ examples/evaluate/eval_downstream_tasks.ps1
@@ -1,8 +1,8 @@
 
-$MODEL = "./results/Doge_Eval/197M/checkpoint-13400"
+$MODEL = "JingzeShi/Doge-60M"
 $OUTPUT_DIR = "./lighteval_results"
 
 lighteval accelerate --override_batch_size 16 `
     --model_args "pretrained=$MODEL" `
     --output_dir $OUTPUT_DIR `
-    --tasks "leaderboard|mmlu|0|1,lighteval|triviaqa|0|1,lighteval|arc:easy|0|1,lighteval|piqa|0|1,leaderboard|hellaswag|0|1,lighteval|openbookqa|0|1,leaderboard|winogrande|0|1"
+    --tasks "original|mmlu|5|1,lighteval|triviaqa|5|1,lighteval|arc:easy|5|1,lighteval|piqa|5|1,leaderboard|hellaswag|5|1,lighteval|openbookqa|5|1,leaderboard|winogrande|5|1"
