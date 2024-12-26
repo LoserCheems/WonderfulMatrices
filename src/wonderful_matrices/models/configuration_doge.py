c87898e63c1a7533affc14daeb04ce419ca8c69d
LoserCheems <3314685395@qq.com>
2024-12-26 14:28:57 +0800

    updated annotation

--- src/wonderful_matrices/models/configuration_doge.py
+++ src/wonderful_matrices/models/configuration_doge.py
@@ -115,9 +115,9 @@ class DogeConfig(PretrainedConfig):
             Whether to use the Cross Domain Mixture of Experts, if `True`, the MoE will inherit the MLP to initialize
         num_cdmmoe_experts (`int`, *optional*, defaults to 2048):
             Number of Private Experts for the Cross Domain Mixture of Experts.
-        num_cdmmoe_heads (`int`, *optional*, defaults to 2):
+        num_cdmmoe_heads (`int`, *optional*, defaults to 4):
             Number of heads of Private Experts for the Cross Domain Mixture of Experts.
-        num_cdmmoe_experts_per_head (`int`, *optional*, defaults to 4):
+        num_cdmmoe_experts_per_head (`int`, *optional*, defaults to 8):
             Number of Private Experts per head for the Cross Domain Mixture of Experts.
         expert_retrieval_size (`int`, *optional*, defaults to 256):
             Dimension of the Expert retrieval states for the Cross Domain Mixture of Experts.
@@ -156,8 +156,8 @@ class DogeConfig(PretrainedConfig):
         attention_dropout=0.0,
         is_moe=False,
         num_cdmmoe_experts=2048,
-        num_cdmmoe_heads=2,
-        num_cdmmoe_experts_per_head=4,
+        num_cdmmoe_heads=4,
+        num_cdmmoe_experts_per_head=8,
         expert_retrieval_size=256,
         **kwargs,
     ):
