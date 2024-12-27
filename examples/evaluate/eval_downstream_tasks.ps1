f100cdbc47ce83c0b99ccd0dee19338de5086b61
LoserCheems <3314685395@qq.com>
2024-12-27 11:28:45 +0800

    updated arc

--- examples/evaluate/eval_downstream_tasks.ps1
+++ examples/evaluate/eval_downstream_tasks.ps1
@@ -1,8 +1,8 @@
 
-$MODEL = "JingzeShi/Doge-60M"
+$MODEL = "JingzeShi/Doge-20M"
 $OUTPUT_DIR = "./lighteval_results"
 
 lighteval accelerate --override_batch_size 1 `
     --model_args "pretrained=$MODEL" `
     --output_dir $OUTPUT_DIR `
-    --tasks "original|mmlu|5|1,lighteval|triviaqa|5|1,lighteval|arc:easy|5|1,lighteval|piqa|5|1,leaderboard|hellaswag|5|1,lighteval|openbookqa|5|1,leaderboard|winogrande|5|1"
+    --tasks "original|mmlu|5|1,lighteval|triviaqa|5|1,lighteval|arc:easy|5|1,leaderboard|arc:challenge|5|1,lighteval|piqa|5|1,leaderboard|hellaswag|5|1,lighteval|openbookqa|5|1,leaderboard|winogrande|5|1"
