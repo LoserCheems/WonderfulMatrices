73858ebcbf349747119bce364729cabcce59f7d0
LoserCheems <3314685395@qq.com>
2025-01-14 13:36:24 +0800

    Update tasks for Doge-Instruct

--- examples/evaluation/eval_downstream_tasks.ps1
+++ examples/evaluation/eval_downstream_tasks.ps1
@@ -2,7 +2,17 @@
 $MODEL = "JingzeShi/Doge-20M"
 $OUTPUT_DIR = "./lighteval_results"
 
-lighteval accelerate --override_batch_size 1 `
-    --model_args "pretrained=$MODEL" `
+if ($MODEL -match "Instruct$") {
+    lighteval accelerate --override_batch_size 1 `
+    --model_args "pretrained=$MODEL,max_model_length=2048" `
     --output_dir $OUTPUT_DIR `
-    --tasks "original|mmlu|5|0,lighteval|triviaqa|5|0,lighteval|arc:easy|5|0,leaderboard|arc:challenge|5|0,lighteval|piqa|5|0,leaderboard|hellaswag|5|0,lighteval|openbookqa|5|0,leaderboard|winogrande|5|0"
+    --tasks "extended|ifeval|5|0" `
+    --use_chat_template `
+    --save_details
+} else {
+    lighteval accelerate --override_batch_size 1 `
+    --model_args "pretrained=$MODEL,max_model_length=2048" `
+    --output_dir $OUTPUT_DIR `
+    --tasks "original|mmlu|5|0,lighteval|triviaqa|5|0,lighteval|arc:easy|5|0,leaderboard|arc:challenge|5|0,lighteval|piqa|5|0,leaderboard|hellaswag|5|0,lighteval|openbookqa|5|0,leaderboard|winogrande|5|0" `
+    --save_details
+}
