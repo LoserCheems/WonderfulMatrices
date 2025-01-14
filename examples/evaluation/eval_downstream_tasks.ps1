dc5decea82b504063d16d0add1e9d23bd5a22349
LoserCheems <3314685395@qq.com>
2025-01-14 15:58:44 +0800

    Update eval command for lighteval 0.7.0

--- examples/evaluation/eval_downstream_tasks.ps1
+++ examples/evaluation/eval_downstream_tasks.ps1
@@ -3,16 +3,14 @@ $MODEL = "JingzeShi/Doge-20M"
 $OUTPUT_DIR = "./lighteval_results"
 
 if ($MODEL -match "Instruct$") {
-    lighteval accelerate --override_batch_size 1 `
-    --model_args "pretrained=$MODEL,max_model_length=2048" `
-    --output_dir $OUTPUT_DIR `
-    --tasks "extended|ifeval|5|0" `
-    --use_chat_template `
-    --save_details
+    lighteval accelerate "pretrained=$MODEL,max_length=2048,trust_remote_code=True" `
+    "extended|ifeval|5|0" `
+    --override-batch-size 1 `
+    --output-dir $OUTPUT_DIR `
+    --use-chat-template
 } else {
-    lighteval accelerate --override_batch_size 1 `
-    --model_args "pretrained=$MODEL,max_model_length=2048" `
-    --output_dir $OUTPUT_DIR `
-    --tasks "original|mmlu|5|0,lighteval|triviaqa|5|0,lighteval|arc:easy|5|0,leaderboard|arc:challenge|5|0,lighteval|piqa|5|0,leaderboard|hellaswag|5|0,lighteval|openbookqa|5|0,leaderboard|winogrande|5|0" `
-    --save_details
+    lighteval accelerate "pretrained=$MODEL,max_model_length=2048,trust_remote_code=True" `
+    "original|mmlu|5|0,lighteval|triviaqa|5|0,lighteval|arc:easy|5|0,leaderboard|arc:challenge|5|0,lighteval|piqa|5|0,leaderboard|hellaswag|5|0,lighteval|openbookqa|5|0,leaderboard|winogrande|5|0" `
+    --override-batch-size 1 `
+    --output-dir $OUTPUT_DIR
 }
